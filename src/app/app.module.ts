4aa1ddbd8af5a4ddee6d6d770f61c5a1e2aa7003
Alok kumar <alokrajnand@gmail.com>
2021-02-04 20:51:19 +0530

    basic laout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './pages/shared/header/header.component';
+import { FooterComponent } from './pages/shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent,   
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,   
   ],
   imports: [
     BrowserModule,
