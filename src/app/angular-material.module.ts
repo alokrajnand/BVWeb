226bbd91386222a33d4f8ca6100b7359306c5dd0
Alok kumar <alokrajnand@gmail.com>
2021-02-04 22:06:22 +0530

    last

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 
 
 import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 var modules = [
         BrowserAnimationsModule,
         MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
 ];
 
 
